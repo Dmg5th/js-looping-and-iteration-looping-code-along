6b055e3cf6235ee31171e85fc668e23d57ced353
Dmg5th <dmgould310@gmail.com>
2020-12-28 14:52:32 -0500

    commit tree 73f71ab5762489fff745ca12ff5c34ce7266a007

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names, event){
+    let newarr = [];
+    for (let i = 0; i < names.length; i++)
+    newarr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    return newarr
+}
+
+function countDown(integer){
+    while (0 <= integer){
+        console.log(integer)
+        integer -= 1;
+    }
+}
